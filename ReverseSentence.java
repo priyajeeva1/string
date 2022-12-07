f572431e870eda43e61d5682653d1e79e794db03
priyajeeva1 <120002930+priyajeeva1@users.noreply.github.com>
2022-12-07 12:22:27 +0530

    Add files via upload

---
+++ ReverseSentence.java
@@ -0,0 +1,16 @@
+public class ReverseSentence
+{
+public static void main(String args[])
+{
+String s="i am java";
+String x[]=s.split(" ");
+//String reverse=" ";
+
+for (int i=0;i>x.length;i++)
+{
+String reverse=" ";
+}
+System.out.println();
+//System.out.println(reverse);
+}
+}
